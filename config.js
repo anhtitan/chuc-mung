98c4be212b0c106ae60ba6e4178bb23563f49110
Phạm Đức Anh <phamducanhofficial@gmail.com>
2023-10-19 17:12:52 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/100001285178636';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Bùi Thị Kiều';
+const contentCard = 'Chúc Bùi Thị Kiều 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = 'bui-thi-kieu.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
